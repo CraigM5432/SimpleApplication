75feb753c84f00164b670c3a417f1315ca1bd74c
CraigM5432 <116079017+CraigM5432@users.noreply.github.com>
2026-03-09 17:22:25 +0000

    added basic Moch test for CI pipeline

---
+++ test/app.test.js
@@ -0,0 +1,7 @@
+const assert = require("assert");
+
+describe("SimpleApplication basic test", function () {
+  it("should confirm that the test framework is working", function () {
+    assert.strictEqual(1 + 1, 2);
+  });
+});
